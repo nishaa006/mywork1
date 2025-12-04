78146fb92ca6e5c02c6bc9868d66fedfc2eb7d9a
nishaa006 <gma0609@mail.ru>
2025-12-05 02:00:13 +0300

    Сделала мобильную адаптацию сайта

--- index.js
+++ index.js
@@ -1,61 +1,56 @@
 
-let password = 'кошки';
-let ask = prompt('Введите пароль');
-if (ask === password){
-    alert('Пароль введен верно');
-} else {
-    alert('Пароль введен неправильно');
-}
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
 
+// Задание 2
+let iPhoneYear = 2007;
+alert(iPhoneYear);
 
-let c = 11;
-if (c > 0 && c < 10){
-    alert('Верно');
-} else {
-    alert('Неверно');
-}
+// Задание 3
+let jsCreator = "Брендан Эйх";
+alert(jsCreator);
 
-let d = 15;
-let e = 9;
-if (d > 100 || e > 100){
-    alert('Верно');
-} else {
-    alert('Неверно');
-}
+// Задание 4
+let num1 = 10;
+let num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2); 
+alert(num1 / num2);
 
+// Задание 5
+let result = 2 ** 5;
+alert(result);
 
-let a = '2';
-let b = '3';
-alert(Number(a) + Number(b));
+// Задание 6
+let A = 9;
+let B = 2;
+alert(A % B);
 
-let season = prompt('Введите номер месяца в году'); // исправлено опечатку
-let monthNumber = parseInt(season);
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
 
-if (monthNumber >= 1 && monthNumber <= 12){
-    switch (monthNumber){
-        case 12:
-        case 1:
-        case 2:
-            alert('Зима');
-            break;
-        case 3:
-        case 4:
-        case 5:
-            alert('Весна');
-            break;
-        case 6:
-        case 7:
-        case 8:
-            alert('Лето');
-            break;
-        case 9:
-        case 10:
-        case 11:
-            alert('Осень');
-            break;
-        default:
-            alert("Неверный номер месяца");
-    }
-} else {
-    alert("Введите число от 1 до 12");
-}
+// Задание 8
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+// Задание 9
+let user = {
+    name: 'Анна',
+    age: 25,
+    isAdmin: true
+};
+
+// Задание 10
+let userName = prompt('Ваше имя:');
+alert(`Привет, ${userName}!`);
