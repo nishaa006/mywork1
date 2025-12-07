b2813d9f804c047d546b138209d207718715e88b
nishaa006 <gma0609@mail.ru>
2025-12-07 05:00:00 +0300

    выполнено

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
-
 // Задание 1
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
+let password = 'пароль';
+let userInput = prompt('Введите пароль');
+
+if (userInput === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
 
 // Задание 2
-let iPhoneYear = 2007;
-alert(iPhoneYear);
+let c = 0;
+
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+c = 0;
+if (c > 0 && c < 10) console.log('Верно'); else console.log('Неверно');
+
+c = 10;
+if (c > 0 && c < 10) console.log('Верно'); else console.log('Неверно');
+
+c = -3;
+if (c > 0 && c < 10) console.log('Верно'); else console.log('Неверно');
+
+c = 2;
+if (c > 0 && c < 10) console.log('Верно'); else console.log('Неверно');
 
 // Задание 3
-let jsCreator = "Брендан Эйх";
-alert(jsCreator);
+let d = 50;
+let e = 120;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
 
 // Задание 4
-let num1 = 10;
-let num2 = 2;
-alert(num1 + num2);
-alert(num1 - num2);
-alert(num1 * num2); 
-alert(num1 / num2);
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
 
 // Задание 5
-let result = 2 ** 5;
-alert(result);
+let monthNumber = 12;
 
-// Задание 6
-let A = 9;
-let B = 2;
-alert(A % B);
-
-// Задание 7
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
-
-// Задание 8
-let age = prompt('Сколько вам лет?');
-alert(age);
-
-// Задание 9
-let user = {
-    name: 'Анна',
-    age: 25,
-    isAdmin: true
-};
-
-// Задание 10
-let userName = prompt('Ваше имя:');
-alert(`Привет, ${userName}!`);
+switch(monthNumber) {
+    case 1:
+    case 2:
+    case 12:
+        console.log('зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('осень');
+        break;
+    default:
+        console.log('номер месяца должен быть от 1 до 12');
+}
